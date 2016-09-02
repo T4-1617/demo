cafdba8c0ac7ef9e3f5d442a9e15934e2bb13cbb
happyguy42 <happyguy42@herkommer.co.uk>
2016-09-02 11:25:23 +0200

    Add files via upload

---
+++ DemoSept2-2/DemoSept2-2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoSept2_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Slumpmässighet
+            Random r = new Random();
+
+            for (int i = 0; i < 20; i++)
+            {
+                //Ger slumptal från 0 till 9
+                Console.WriteLine(r.Next(0,10));
+            }
+        }
+    }
+}
