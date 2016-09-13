120c69e14ae1778b802f24a8b7d73afdca17a7bc
happyguy42 <happyguy42@herkommer.co.uk>
2016-09-13 09:36:15 +0200

    Add files via upload

---
+++ Demo0913-01/Demo0913-01/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace Demo0913_01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 45);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 108);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(29, 26);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(80, 17);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.checkBox1);
+            this.panel1.Location = new System.Drawing.Point(153, 53);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 100);
+            this.panel1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(29, 61);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(365, 216);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Demo0913-01/Demo0913-01/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Demo0913_01
+{
+    public partial class Form1 : Form
+    {
+        System.Collections.ArrayList MyCustomerList = new System.Collections.ArrayList();
+        public Form1()
+        {
+            InitializeComponent();
+
+            //Göm panelen
+            panel1.Visible = false;
+
+
+            //Skapa lite exempelkunder för denna demo
+            Customer c = new Customer();
+            c.Name = "Musse Pigg";
+            c.VIP = false;
+
+            MyCustomerList.Add(c);
+
+            //Alternativt sätt, lite kortare
+            //Här skapas/instansieras objektet OCH egenskaperna sätts direkt
+            Customer d = new Customer() { Name = "Janne Långben", VIP = false };
+            MyCustomerList.Add(d);
+
+            //Alternativt sätt, ännu kortare
+            //Här görs båda sakerna på samma rad, dvs skapa kund objekt OCH lägga till i kundlistan
+            MyCustomerList.Add(new Customer() { Name = "Kalle Anka", VIP = false });
+
+            MyCustomerList.Add(new Customer() { Name = "Arne Anka", VIP = false });
+            MyCustomerList.Add(new Customer() { Name = "Kajsa Anka", VIP = false });
+
+
+            //Visa alla kunderna i listboxen
+            listBox1.Items.Clear();
+            foreach (Customer item in MyCustomerList)
+            {
+                listBox1.Items.Add(item);
+                //Obs: detta visar texten "Demo0913-01.Customer" vilket är korrekt
+                //Vi har inte talat om för listboxen VAD den ska visa från ett Customer-objekt
+                //och detta är default, kallas för metoden .ToString()
+                //då skrivs namnespace.klassnamn ut
+
+                //För att fixa detta, talar vi om för listboxen VAD den ska använda för att visa text
+                //Det MÅSTE vara en PROPERTY från Customerklassen
+                listBox1.DisplayMember = "Name";
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Här hamnar vi om vi trycker på knappen (Spara ändring av VIP status)
+            Customer c;
+            c = (Customer)listBox1.SelectedItem;
+            c.VIP = checkBox1.Checked;
+
+            //Uppdatera listboxen
+            listBox1.Items.Clear();
+            foreach (Customer item in MyCustomerList)
+            {
+                listBox1.Items.Add(item);
+                listBox1.DisplayMember = "Name";
+            }
+
+            //Göm panelen igen
+            panel1.Visible = false;
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //Visa panelen
+            panel1.Visible = true;
+
+            //Hugg tag i den kund (Customer objektet) som valts i listan
+            Customer c;
+            c = (Customer)listBox1.SelectedItem; //Måste CASTAS, dvs se till att rätt klass används, mer om detta imorgon
+
+            //Skriv ut info om VIP
+            checkBox1.Checked = c.VIP;
+
+        }
+    }
+}
