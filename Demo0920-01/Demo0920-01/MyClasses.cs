2f0ee456ed600adb526f64b2f601d2aa346ec68b
happyguy42 <happyguy42@herkommer.co.uk>
2016-09-20 10:07:12 +0200

    Add files via upload

---
+++ Demo0920-01/Demo0920-01/MyClasses.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo0920_01
+{
+    public class Customer : Person
+    {
+        public int CustomerId { get; set; }
+        public bool VIP { get; set; }
+    }
+
+    public class Employee : Person
+    {
+        public string Title { get; set; }
+        public decimal Salary { get; set; }
+
+        //Vi är inte nöjda med den nedärvda egenskapen Fullname
+        //vi vill göra en egen implementation (dvs egen kod)
+        //Vi vill göra en OVERRIDE
+        //För att det ska funka, behöver basklassen tillåta det
+        //Görs via VIRTUAL
+        public override string FullName
+        {
+            get
+            {
+                return string.Format("{0}, {1}", LastName, FirstName);
+            }
+        }
+
+    }
+
+    public abstract class Person
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        //Detta är en READONLY Property
+        //Använda alltid properties istället för metoder (best practice)
+        //I de fall det funkar, dvs presentera data
+        public virtual string FullName
+        {
+            get { return string.Format("{0} {1}", FirstName, LastName); }
+
+        }
+
+    }
+}
---
+++ Demo0920-01/Demo0920-01/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo0920_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p ;
+            p = new Customer();
+            p.FirstName = "Kalle"; //2 properties
+
+            Customer c;
+            c = (Customer)p;
+            c.CustomerId = 100; //4 properties
+            c.FirstName = "Arne";
+            c.LastName = "Anka";
+
+            Console.WriteLine(p.FirstName);
+
+            Console.WriteLine(p.FullName);
+
+            Employee emp;
+            p = new Employee();
+            p.FirstName = "Musse";
+            p.LastName = "Pigg";
+
+            emp = (Employee)p;
+            Console.WriteLine(emp.FullName);
+            Console.WriteLine(p.FullName);
+            Console.WriteLine(c.FullName);
+        }
+    }
+}
