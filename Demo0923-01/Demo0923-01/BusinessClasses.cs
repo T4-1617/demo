8d0e9a9f811fb793ab4a062206ea4c8f82b0d75a
happyguy42 <happyguy42@herkommer.co.uk>
2016-09-23 14:21:55 +0200

    Add files via upload

---
+++ Demo0923-01/Demo0923-01/BusinessClasses.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo0923_01
+{
+    public class Customer
+    {
+        private System.Collections.ArrayList accounts;
+
+        public Customer()
+        {
+            accounts = new System.Collections.ArrayList();
+        }
+
+        public string Name { get; set; }
+        public int Id { get; set; }
+
+        public Account GetAccount(int AccountIndex)
+        {
+            return (Account)accounts[AccountIndex];
+        }
+
+        public void CreateAccount(decimal InitialDeposit)
+        {
+            if (InitialDeposit >= 1000)
+            {
+
+                Account a = new Account();
+                a.Deposit(InitialDeposit);
+
+                accounts.Add(a); 
+            }
+        }
+    }
+
+
+    public class Account
+    {
+        private decimal balance;
+        public int Number { get; set; }
+
+        public decimal Balance
+        {
+            get { return balance; }
+        }
+
+
+        public void Deposit(decimal amount)
+        {
+            balance = balance + amount;
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            balance = balance - amount;
+        }
+    }
+}
---
+++ Demo0923-01/Demo0923-01/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Demo0923_01
+{
+    public partial class Form1 : Form
+    {
+        System.Collections.ArrayList Customers;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            Customers = new System.Collections.ArrayList();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            decimal initialDeposit = decimal.Parse(textBox1.Text);
+
+            Customer c = new Customer();
+            //c.CreateAccount(1000);
+            c.CreateAccount(initialDeposit);
+
+            Customers.Add(c);
+
+            MessageBox.Show("Saldo: " + c.GetAccount(0).Balance);
+        }
+    }
+}
