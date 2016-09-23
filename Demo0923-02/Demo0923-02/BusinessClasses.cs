e73378969d1a23899c1e0c13b9a09c57eb102d82
happyguy42 <happyguy42@herkommer.co.uk>
2016-09-23 15:33:39 +0200

    Add files via upload

---
+++ Demo0923-02/Demo0923-02/BusinessClasses.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo0923_02
+{
+    public class Customer
+    {
+        private System.Collections.ArrayList accounts;
+
+        public Customer()
+        {
+            accounts = new System.Collections.ArrayList();
+        }
+
+        public string Name { get; set; }
+        public int Id { get; set; }
+
+        public Account GetAccount(int AccountIndex)
+        {
+            return (Account)accounts[AccountIndex];
+        }
+
+        public System.Collections.ArrayList GetAccounts()
+        {
+            return accounts;
+        }
+
+        public void CreateAccount(decimal InitialDeposit)
+        {
+            if (InitialDeposit >= 1000)
+            {
+
+                Account a = new Account();
+                a.Deposit(InitialDeposit);
+
+                accounts.Add(a);
+            }
+        }
+    }
+
+
+    public class Account
+    {
+        private decimal balance;
+        public int Number { get; set; }
+
+        public decimal Balance
+        {
+            get { return balance; }
+        }
+
+      
+
+        public string AccountInfo
+        {
+            get { return string.Format("Konto har {0} kr", Balance); }
+           
+        }
+
+
+        public void Deposit(decimal amount)
+        {
+            balance = balance + amount;
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            balance = balance - amount;
+        }
+    }
+}
---
+++ Demo0923-02/Demo0923-02/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Demo0923_02
+{
+    public partial class Form1 : Form
+    {
+        private System.Collections.ArrayList _customers;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            _customers = new System.Collections.ArrayList();
+
+            //Demo dummies
+            Customer c = new Customer() { Name = "Kalle" };
+            c.CreateAccount(1200);
+            _customers.Add(c);
+
+            c = new Customer() { Name = "Musse" };
+            c.CreateAccount(2200);
+            c.CreateAccount(4310);
+            _customers.Add(c);
+
+            //Visa alla kunder i listbox 1
+            listBox1.DisplayMember = "Name";
+            foreach (Customer item in _customers)
+            {
+                listBox1.Items.Add(item);
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //Visa alla konton för vald kund
+
+            Customer c = (Customer)listBox1.SelectedItem;
+            DisplayAccounts(c);
+        }
+
+        private void DisplayAccounts(Customer c)
+        {
+            listBox2.Items.Clear();
+            listBox2.DisplayMember = "AccountInfo";
+            foreach (Account item in c.GetAccounts())
+            {
+                listBox2.Items.Add(item);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Sätt in pengar på valt konto
+            if (listBox2.SelectedItem != null)
+            {
+                //Dvs ett konto har valts i listan
+
+                Account a = (Account)listBox2.SelectedItem;
+                a.Deposit(decimal.Parse(textBox1.Text));
+
+                DisplayAccounts((Customer)listBox1.SelectedItem);
+            }
+        }
+    }
+}
