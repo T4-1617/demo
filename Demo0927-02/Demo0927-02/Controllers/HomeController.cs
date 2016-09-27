b32129bde586b7d0fff9827771c0954afc284c0e
happyguy42 <happyguy42@herkommer.co.uk>
2016-09-27 11:37:33 +0200

    Add files via upload

---
+++ Demo0927-02/Demo0927-02/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Demo0927_02.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public string Index2()
+        {
+            return "Hello from Web Server, åäö";
+        }
+
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            //Per default hamnar vi här
+            //pga MS MVC Conventions
+
+            //Var är våran VIEW?
+            //Vad heter vår VIEW?
+            //Per default ska den heta INDEX
+            //dvs samma som denna metod heter
+            return View();
+        }
+    }
+}
